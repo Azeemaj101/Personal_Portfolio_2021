22ed3fcbec3dca043d35f7417fbaee3fe418a1fb
Muhammad Azeem <61928905+Azeemaj101@users.noreply.github.com>
2021-09-27 15:02:54 +0500

    Update 79.2

---
+++ README.md
@@ -0,0 +1,11 @@
+# Web_Porfolio_2021
+![image](https://user-images.githubusercontent.com/61928905/131449022-e06d6387-1546-4655-ae25-077659d8731e.png)
+![image](https://user-images.githubusercontent.com/61928905/131449039-6e63ba2d-d2b2-41de-bcde-f0791fe1fb26.png)
+![image](https://user-images.githubusercontent.com/61928905/131449053-d10ec1cc-2a54-41fd-b048-9fd0c17ec67b.png)
+![image](https://user-images.githubusercontent.com/61928905/131449090-455b6ebf-390e-4821-a073-c6488953f58c.png)
+![image](https://user-images.githubusercontent.com/61928905/131449157-84d47814-e690-4995-adbb-4ac59d5e65a3.png)
+![image](https://user-images.githubusercontent.com/61928905/131449177-9764026a-027e-4892-82fd-786cd90fe36a.png)
+![image](https://user-images.githubusercontent.com/61928905/131449192-f022fc61-7f9b-425b-9f14-1eec59912630.png)
+![image](https://user-images.githubusercontent.com/61928905/131449214-4e5d1e63-17b1-45db-ae33-f33ee26ef3cd.png)
+![Lines of code](https://img.shields.io/tokei/lines/github/azeemaj101/Web_Portfolio_2021?logo=CiviCRM)
+<a href='https://azeemaj101.herokuapp.com/'>Click Me</a>
---
+++ templates/public/script.js
@@ -0,0 +1,236 @@
+function test() {
+    var tabsNewAnim = $('#navbarSupportedContent');
+    var selectorNewAnim = $('#navbarSupportedContent').find('li').length;
+    var activeItemNewAnim = tabsNewAnim.find('.active');
+    var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
+    var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+    var itemPosNewAnimTop = activeItemNewAnim.position();
+    var itemPosNewAnimLeft = activeItemNewAnim.position();
+    $(".hori-selector").css({
+        "top": itemPosNewAnimTop.top + "px",
+        "left": itemPosNewAnimLeft.left + "px",
+        "height": activeWidthNewAnimHeight + "px",
+        "width": activeWidthNewAnimWidth + "px"
+    });
+    $("#navbarSupportedContent").on("click", "li", function(e) {
+        $('#navbarSupportedContent ul li').removeClass("active");
+        $(this).addClass('active');
+        var activeWidthNewAnimHeight = $(this).innerHeight();
+        var activeWidthNewAnimWidth = $(this).innerWidth();
+        var itemPosNewAnimTop = $(this).position();
+        var itemPosNewAnimLeft = $(this).position();
+        $(".hori-selector").css({
+            "top": itemPosNewAnimTop.top + "px",
+            "left": itemPosNewAnimLeft.left + "px",
+            "height": activeWidthNewAnimHeight + "px",
+            "width": activeWidthNewAnimWidth + "px"
+        });
+    });
+}
+$(document).ready(function() {
+    setTimeout(function() { test(); });
+});
+$(window).on('resize', function() {
+    setTimeout(function() { test(); }, 500);
+});
+$(".navbar-toggler").click(function() {
+    setTimeout(function() { test(); });
+});
+
+var typed = new Typed('.box', {
+    strings: [
+        "CREATIVE DEVELOPER",
+        "Web Developer",
+        "Python Developer",
+        "JS Developer",
+        "JAVA Developer",
+        "Designer",
+        "Photographer"
+    ],
+    typeSpeed: 40,
+    backspaceSpeed: 20,
+    backspaceDelay: 80,
+    repeatDelay: 10,
+    repeat: true,
+    autoStart: true,
+    startDelay: 10,
+    loop: true,
+});
+var loader;
+
+document.getElementById('myVideo').addEventListener('ended', myHandler, false);
+
+function myHandler(e) {
+    // What you want to do after the event
+    displayContent();
+
+}
+
+// function loadNow(opacity) {
+//     if (opacity <= 0) {
+//         displayContent();
+//     } else {
+//         loader.style.opacity = opacity;
+//         window.setTimeout(function() {
+//             loadNow(opacity - 0.10);
+//         }, 300);
+//     }
+// }
+var count1 = 0;
+
+function displayContent() {
+    document.getElementsByClassName('content')[0].style.display = 'block';
+    document.getElementsByClassName('type')[0].style.display = 'block';
+    document.getElementsByClassName('content1')[1].style.display = 'block';
+    document.getElementsByClassName('content1')[2].style.display = 'block';
+    document.getElementsByClassName('content1')[3].style.display = 'block';
+    document.getElementsByClassName('content1')[4].style.display = 'block';
+    document.getElementsByClassName('arrow')[0].style.display = 'block';
+    document.getElementById('loader').style.display = 'none';
+    gsap.from('.content', {
+        x: -60,
+        y: -60,
+        ease: "bounce",
+        duration: 2,
+        delay: .1,
+        rotation: "45_ccw"
+    })
+    gsap.from('.content1', {
+        y: 50,
+        duration: 2,
+        scale: 1
+    })
+    gsap.from('.type', {
+        x: 200,
+        duration: 2,
+        scale: 1,
+
+    })
+    gsap.from('.arrow', {
+        y: -20,
+        duration: 2,
+        scale: 1,
+
+    })
+    count1 = 1;
+}
+// document.addEventListener("DOMContentLoaded", function() {
+//     loader = document.getElementById('loader');
+//     loadNow(1);
+
+// });
+let lastScrollTop = 0;
+let navbar = document.getElementsByTagName('nav')[0];
+var count = 0;
+window.addEventListener("scroll", function(event) {
+    var scroll_y = this.scrollY;
+    var scroll_x = this.scrollX;
+    if (scroll_y >= 310 && window.innerWidth > 690) {
+        document.getElementsByClassName('content')[0].style.display = 'none';
+        document.getElementsByClassName('type')[0].style.display = 'none';
+        document.getElementsByClassName('content1')[1].style.display = 'none';
+        document.getElementsByClassName('content1')[2].style.display = 'none';
+        document.getElementsByClassName('content1')[3].style.display = 'none';
+        document.getElementsByClassName('content1')[4].style.display = 'none';
+        document.getElementsByClassName('arrow')[0].style.display = 'none';
+
+        document.getElementsByClassName('email_line')[0].style.display = 'flex';
+        document.getElementsByClassName('contact_logo')[0].style.display = 'flex';
+        count = 1;
+
+    } else if (scroll_y <= 310 && count === 1) {
+        document.getElementsByClassName('content')[0].style.display = 'block';
+        document.getElementsByClassName('type')[0].style.display = 'block';
+        document.getElementsByClassName('content1')[1].style.display = 'block';
+        document.getElementsByClassName('content1')[2].style.display = 'block';
+        document.getElementsByClassName('content1')[3].style.display = 'block';
+        document.getElementsByClassName('content1')[4].style.display = 'block';
+        document.getElementsByClassName('email_line')[0].style.display = 'none';
+        document.getElementsByClassName('contact_logo')[0].style.display = 'none';
+    }
+    if (scroll_y !== 0) {
+        document.getElementsByClassName('arrow')[0].style.display = 'none';
+    } else if (scroll_y === 0 && count1 === 1) {
+        document.getElementsByClassName('arrow')[0].style.display = 'block';
+    }
+
+    // navbar code
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+        navbar.style.top = "-80px";
+    } else {
+        navbar.style.position = "fixed";
+        navbar.style.width = "100%";
+        navbar.style.zIndex = "1";
+        navbar.style.opacity = "0.9";
+        navbar.style.top = "0px";
+        navbar.style.transition = "0.5s";
+
+    }
+    lastScrollTop = scrollTop;
+    if (scrollY === 0) {
+        navbar.style.opacity = "1";
+    }
+});
+// scrollY = 0;
+if (true) {
+    navbar.style.position = "fixed";
+    navbar.style.width = "100%";
+    navbar.style.zIndex = "1";
+    navbar.style.opacity = "1";
+    navbar.style.top = "0px";
+    navbar.style.transition = "0.5s";
+    document.getElementsByClassName('content')[0].style.display = 'none';
+    document.getElementsByClassName('type')[0].style.display = 'none';
+    document.getElementsByClassName('content1')[1].style.display = 'none';
+    document.getElementsByClassName('content1')[2].style.display = 'none';
+    document.getElementsByClassName('content1')[3].style.display = 'none';
+    document.getElementsByClassName('content1')[4].style.display = 'none';
+    document.getElementsByClassName('arrow')[0].style.display = 'none';
+
+}
+
+
+//  Show More or Show Less Code
+$(".col1").slice(0, 8).show()
+if ($(".col1").length <= 4) {
+    document.getElementsByClassName("btn")[0].style.display = "none";
+}
+$(".btn").on("click", function() {
+    $(".col1:hidden").slice(0, 4).slideDown()
+    if ($(".col1:hidden").length == 0) {
+        // $(".btn").fadeOut('slow')
+        document.getElementsByClassName("btn")[0].style.display = "none";
+        document.getElementsByClassName("btn1")[0].style.display = "block";
+    }
+
+})
+
+function less() {
+    $(".col1").slice(8, ).hide()
+
+}
+$(".btn1").on("click", function() {
+    // $(".col1:hidden").slice(0, 1).slideDown()
+    // if ($(".col1:hidden").length !== 0) {
+    // $(".btn1").fadeOut('slow')
+    document.getElementsByClassName("btn1")[0].style.display = "none";
+    document.getElementsByClassName("btn")[0].style.display = "block";
+    // }
+})
+AOS.init({
+    duration: 1000,
+    once: true
+});
+
+let date = new Date();
+if (`${date.getDate()}-${date.getMonth()+1}` === "1-3") {
+    document.getElementsByClassName('birthday')[0].style.display = 'block';
+} else {
+    document.getElementsByClassName('birthday')[0].style.display = 'none';
+}
+if (`${date.getDate()}-${date.getMonth()+1}` === "1-1") {
+    document.getElementsByClassName('new_year')[0].style.display = 'block';
+} else {
+    document.getElementsByClassName('new_year')[0].style.display = 'none';
+}
